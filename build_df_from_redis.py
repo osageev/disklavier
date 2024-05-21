5be3ec3f60712040383092bfb49850ad92460277
Finlay Miller <finlay@dal.ca>
2024-05-21 14:44:01 -0300

    sim table built

--- build_df_from_redis.py
+++ build_df_from_redis.py
@@ -1,84 +1,119 @@
-from multiprocessing import Process, cpu_count
+import os
+import json
+import time
 import redis
+import numpy as np
+import pandas as pd
 from rich.progress import (
     Progress,
     SpinnerColumn,
     TimeElapsedColumn,
     MofNCompleteColumn,
 )
-import json
-import pandas as pd
-import numpy as np
 
 from typing import List
 
 
-def build_similarity_dataframe(redis_url: str, all_files: List[str], indices: slice, batch_size=1000):
-    r = redis.Redis(redis_url)
-    # initialize an empty DataFrame with row_files as rows and col_files as columns
-    df = pd.DataFrame(index=all_files[indices], columns=all_files, dtype=np.float16)
+def verify_df(row, df, p):
+    print(f"calculating mask...")
+    mask = np.random.rand(*df.shape) < p
+    print("arranging samples...")
+    samples = [
+        (row_idx, col_idx, df.iat[row_idx, col_idx])
+        for row_idx, col_idx in zip(*np.where(mask))
+    ]
+
+    progress = Progress(
+        SpinnerColumn(),
+        *Progress.get_default_columns(),
+        TimeElapsedColumn(),
+        MofNCompleteColumn(),
+        refresh_per_second=1,
+    )
+    check_task = progress.add_task(f"checking files...", total=len(samples))
+
+    with progress:
+        for row, col, val in samples:
+            sim = r.json().get(f"cmp:{row}:{col}:pitch_histogram", "$.sim")
+
+            if sim != val:
+                print(f"value mismatch found at {row}:{col} -> {sim} != {val}")
+
+            progress.advance(check_task)
+
+    print("check complete")
+
+
+def build_similarity_dataframe(all_files: List[str]):
+    r = redis.Redis(host="localhost", port=6379, db=0)
+    df = pd.DataFrame(index=all_files, columns=all_files)
 
     # process keys in batches
-    for i in range(0, indices.stop - indices.start, batch_size):
-        row_batch = all_files[indices][i : i + batch_size]
+    progress = Progress(
+        SpinnerColumn(),
+        *Progress.get_default_columns(),
+        TimeElapsedColumn(),
+        MofNCompleteColumn(),
+        refresh_per_second=1,
+    )
+    update_task = progress.add_task(
+        f"gathering similarities", total=len(all_files)
+    )
 
-        # use Redis pipeline to batch process the keys
-        with r.pipeline() as pipe:
-            for row_file in row_batch:
-                key_pattern = f"cmp:{row_file}:*"
-                keys = r.scan_iter(match=key_pattern)
+    with progress:
+        for i in range(len(all_files)):
+            row_file = all_files[i]
+            keys = [
+                f"cmp:{row_file}:{col_file}:pitch_histogram" for col_file in all_files
+            ]
+
+            with r.pipeline() as pipe:
                 for key in keys:
-                    pipe.get(key)
-            results = pipe.execute()
+                    pipe.execute_command("JSON.GET", key, "$")  # TODO: try MGET
+                results = pipe.execute()
 
-        # parse the results and update the DataFrame
-        for row_file in row_batch:
-            key_pattern = f"cmp:{row_file}:*"
-            keys = list(r.scan_iter(match=key_pattern))
+            # parse the results and update the dataframe
             for key, value in zip(keys, results):
+                result = json.loads(value)[0]
                 if value:
-                    data = json.loads(value)
-                    col_file = data["col_file"]
-                    sim = round(data["sim"], 3)  # limit precision to 3 decimal places
-                    if col_file in col_files:
-                        df.at[row_file, col_file] = sim
+                    if result["col_file"] in all_files:
+                        df.at[row_file, result["col_file"]] = {"sim": result["sim"], "transformations": result["mutations"]}
+                    else:
+                        print(
+                            f"ERROR@key {result['col_file']}: not set on {key}"
+                        )
+                else:
+                    print(f"ERROR@key {key}: no value")
+            progress.advance(update_task)
 
-    return df
+    df.to_feather(
+        os.path.join("outputs", "records", "chunks", "sim.feather")
+    )
 
+if __name__ == "__main__":
+    # connect to redis
+    r = redis.Redis(host="localhost", port=6379, db=0)
 
-if __name__=="__main__":
-    # Connect to Redis
-    redis_url = "redis://localhost:6379"
-    r = redis.Redis(redis_url)
+    # load filenames
+    properties_path = "data/outputs/careful.json"
+    properties = {}
+    with open(properties_path, "r") as f:
+        properties = json.load(f)
+    names = list(properties.keys())
+    names.sort()
 
-    total_keys = r.dbsize()
-    num_processes = cpu_count()
-    keys_per_process = total_keys // num_processes  # type: ignore
-    extra_keys = total_keys % num_processes  # type: ignore
+    st = time.time()
+    build_similarity_dataframe(names)
+    et = time.time()
 
-    processes = []
-    start_index = 0
-    for i in range(num_processes):
-        # Distribute the extra keys among the first few processes
-        end_index = start_index + keys_per_process + (1 if i < extra_keys else 0)
-        process = Process(
-            target=build_similarity_dataframe, args=(redis_url, start_index, end_index, i)
-        )
-        processes.append(process)
-        process.start()
-        start_index = end_index
+    big_df = pd.read_feather(
+        os.path.join("outputs", "records", "chunks", "sim.feather")
+    )
 
-    # Wait for all processes to complete
-    for process in processes:
-        subr_i = process.join()
-        print(f"[MAIN]   subroutine {subr_i} complete")
+    memory_usage = big_df.memory_usage(index=True).sum()
+    print(
+        f"[MAIN] Time taken to generate DataFrame: {et - st:.2f}s"
+    )
+    print(f"[MAIN] Memory usage of DataFrame: {memory_usage / (1024 * 1024):.2f} MB")
 
-    # Example list of all possible row_files and col_files (for demonstration purposes)
-    row_files = [f"20231220-80-01_0000-{i:04d}.mid" for i in range(4000)]
-    col_files = [f"20240117-64-06_0080-{i:04d}.mid" for i in range(4000)]
-
-    # Build the DataFrame
-    similarity_df = build_similarity_dataframe(redis_url, row_files, batch_size=100)
-
-    # Print the DataFrame
-    print(similarity_df)
+    # verify_df(r, big_df, 10)
